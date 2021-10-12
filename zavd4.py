05de4080648085fb256244edd072da2d49afd6a3
Roman Papish <roman.papish@gmail.com>
2021-10-12 20:34:41 +0300

    Add zavd4.py

---
+++ zavd4.py
@@ -0,0 +1,13 @@
+#Завдання прийняти прізвище і ім'я, і вивести їх в зворотньому порядку
+#namelist=[] 
+#name=input()
+#lastname=input()
+#namelist.append(name)
+#namelist.append(lastname)
+#print("Firstname and Lastname of user: ", namelist)
+#namelist.reverse()
+#print("Reverse firstname and lastname of user: ", namelist)
+def name1(name,lastname):
+	print(lastname,name)
+
+name1("roman","papish")
