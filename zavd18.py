88900b8c9f177399f5e22cb5a1039b6f6adbfbc4
Roman Papish <roman.papish@gmail.com>
2021-12-09 18:20:36 +0200

    Add zavd18.py

---
+++ zavd18.py
@@ -0,0 +1,8 @@
+#Write aPython program to test whether a passed letter is vowel or not
+numb=input('input any numder: ')
+teststr=['1','5','8','3']
+if numb in teststr:
+	print('true!!!')
+else:
+	print('false')
+
