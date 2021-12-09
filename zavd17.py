56cbd2d0edfcdc6d9ffafce34833c92ee0fcda3f
Roman Papish <roman.papish@gmail.com>
2021-12-09 18:20:06 +0200

    Add zavd17.py

---
+++ zavd17.py
@@ -0,0 +1,8 @@
+#Write aPython program to test whether a passed letter is vowel or not
+littre=input('input any littre: ')
+vowel=['a','e','y','u','i','o',]
+if littre in vowel:
+	print('Littre vowel!!!')
+else:
+	print('Littre not vowel')
+
